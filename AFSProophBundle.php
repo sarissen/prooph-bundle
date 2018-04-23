73a08849edfe3a6f8dee0111921a2fa016a23517
Sven Arissen <sven.arissen@beetree.nl>
2018-04-23 16:09:58 +0200

    Added compiler pass registration

--- AFSProophBundle.php
+++ AFSProophBundle.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace AFS\ProophBundle;
 
 
+use AFS\ProophBundle\DependencyInjection\Compiler\EventPublisherPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,6 +16,8 @@ class AFSProophBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+
+        $container->addCompilerPass(new EventPublisherPass());
     }
 
 }
