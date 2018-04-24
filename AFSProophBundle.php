43f8c4acde43fd54f09249b3ce4d4a66caaf80df
Sven Arissen <sven.arissen@beetree.nl>
2018-04-24 09:13:55 +0200

    Removed loading of EventPublisherPass

--- AFSProophBundle.php
+++ AFSProophBundle.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 namespace AFS\ProophBundle;
 
 
-use AFS\ProophBundle\DependencyInjection\Compiler\EventPublisherPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,8 +15,6 @@ class AFSProophBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
-        $container->addCompilerPass(new EventPublisherPass());
     }
 
 }
